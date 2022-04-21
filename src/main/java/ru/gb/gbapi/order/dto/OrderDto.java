da00dc2ce33d610f888aa61aeeaffd675559e4a8
Ilay Vinogradov <ilay125114@gmail.com>
2022-04-21 23:05:57 +0300

    lesson 7

---
+++ src/main/java/ru/gb/gbapi/order/dto/OrderDto.java
@@ -0,0 +1,55 @@
+package ru.gb.gbapi.order.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.gb.gbapi.common.enums.OrderStatus;
+import ru.gb.gbapi.product.dto.ProductDto;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderDto implements Serializable {
+
+	static final long serialVersionUID = -7048580037193194357L;
+
+	private Long id;
+	private String address;
+	@NotBlank
+	private String firstname;
+	@NotBlank
+	private String lastname;
+	@NotBlank
+	private String phone;
+	@Email
+	private String mail;
+	private OrderStatus status;
+	@FutureOrPresent
+	@JsonFormat(pattern="dd.MM.yyyy")
+	@JsonDeserialize(using = LocalDateDeserializer.class)
+	@JsonSerialize(using = LocalDateSerializer.class)
+	private LocalDate deliveryDate;
+	@NotEmpty
+	private Set<ProductDto> products;
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	private LocalDateTime createdDate;
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	private LocalDateTime lastModifiedDate;
+}
